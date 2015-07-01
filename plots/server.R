0ea00f3de2da530c12842cf9512735dd2f5e3df9
lbedogni <lbedogni@cs.unibo.it>
2015-07-01 09:56:30 +0200

    Bugfixes

--- plots/server.R
+++ plots/server.R
@@ -5,17 +5,21 @@ shinyServer(
 	function(input, output) {
 		camps <- list.files("data/", pattern = "*parsed*")
 		output$campSelector <- renderUI({
-			   selectInput("vaar", "Choose Option:", as.list(camps)) 
+			#TODO Would be nice to give the possibility to plot more files on the same chart
+			# This would involve a for loop through the different files
+			selectInput("vaar", "Choose Option:", as.list(camps)) 
 		})
 
 		dataf <- as.data.frame(read.table("data/test-BigBuckBunny_1sec_parsed"))
 		dataInput <- reactive({
 #			read.table("data/testProvaOK.log-BigBuckBunny_2sec_parsed")
+			#TODO Add names to the columns
 			read.table(paste("data/",input$vaar,sep = ""))
 #			print(input$vaar)
 #			print(input$var2)
 #			dataf <- as.data.frame(dataInput())
 		})
+
 		selectedData <- reactive({
 #			print(input$xcol)
 #			print(input$ycol)
@@ -23,6 +27,7 @@ shinyServer(
 #			print(dataf[,c(input$xcol)])
 			dataf <- dataInput()
 			print(dataf)
+			#print(input)
 			dataf[, c(input$xcol, input$ycol)]
 		})
 
@@ -32,6 +37,6 @@ shinyServer(
 #		dd = data.frame(x = df$V6, y = df$V3)
 		
 		output$chart <- renderPlot({
-			plot(selectedData(), type="line")
+			plot(selectedData(), type="l")
 		})
 })
