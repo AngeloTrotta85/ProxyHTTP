f4a36a1b387a496849429327b6f361150e6926e8
Luca Bedogni <lbedogni@cs.unibo.it>
2015-09-16 12:29:29 +0200

    Added the tvws scripts

---
+++ tvws/trace.sh
@@ -0,0 +1,600 @@
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 423232.062092
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 408514.4307
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 392633.750861
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 375735.013049
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 357958.579463
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 339437.892526
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 320297.838495
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 300653.700844
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 280610.616694
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 260263.441603
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 239696.929765
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 218986.144884
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 198197.028585
+./tc.sh set wlan0 248393.614453
+sleep 4
+./tc.sh set eth0 177387.066112
+./tc.sh set wlan0 243986.79887
+sleep 4
+./tc.sh set eth0 156606.001731
+./tc.sh set wlan0 239586.102884
+sleep 4
+./tc.sh set eth0 135896.567706
+./tc.sh set wlan0 235200.575678
+sleep 4
+./tc.sh set eth0 115295.20063
+./tc.sh set wlan0 230837.933239
+sleep 4
+./tc.sh set eth0 96166.2030166
+./tc.sh set wlan0 226504.703534
+sleep 4
+./tc.sh set eth0 80644.4167659
+./tc.sh set wlan0 222206.362726
+sleep 4
+./tc.sh set eth0 65265.0494274
+./tc.sh set wlan0 217947.461001
+sleep 4
+./tc.sh set eth0 50041.6030233
+./tc.sh set wlan0 213731.737382
+sleep 4
+./tc.sh set eth0 34985.0249396
+./tc.sh set wlan0 209562.223451
+sleep 4
+./tc.sh set eth0 20104.0435964
+./tc.sh set wlan0 205441.33631
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 201370.961322
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 197352.525365
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 193387.061372
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 189475.26498
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 185617.544079
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 181814.062032
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 178064.775274
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 174369.465955
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 170727.770215
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 167139.202639
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 163603.177368
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 160119.02629
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 124981.258261
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 122053.526739
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 119166.322602
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 116318.771167
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 113510.011731
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 110739.198691
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 108005.502412
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 105308.109892
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 102646.225253
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100019.070077
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 250000
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 249304.565001
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 248408.120838
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 247500.548118
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 246582.272216
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 93553.7127603
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 92615.2833132
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 91667.3910855
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 90710.4366733
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 89744.8138263
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 240870.90924
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 239889.102376
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 238899.765302
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 237903.262562
+sleep 4
+./tc.sh set eth0 432000
+./tc.sh set wlan0 236899.951061
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 83790.1799761
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 82774.2906871
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 81752.6167233
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 80725.4837304
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 79693.2094536
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 78656.1037365
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 77614.4685338
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 76568.5979391
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 75518.7782237
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 74465.2878886
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 73408.3977264
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 72348.3708929
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 71285.4629884
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 70219.9221466
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 69151.9891314
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 68081.8974399
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 67009.8734112
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 65936.1363416
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 64860.8986025
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 63784.3657645
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 62706.7367232
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 61628.2038287
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 60548.9530176
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 59469.1639467
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 58389.0101283
+sleep 4
+./tc.sh set eth0 345601.405927
+./tc.sh set wlan0 209408.659066
+sleep 4
+./tc.sh set eth0 340499.579975
+./tc.sh set wlan0 208328.272394
+sleep 4
+./tc.sh set eth0 335398.322057
+./tc.sh set wlan0 207248.006012
+sleep 4
+./tc.sh set eth0 330298.341942
+./tc.sh set wlan0 206168.010223
+sleep 4
+./tc.sh set eth0 325200.323622
+./tc.sh set wlan0 205088.429872
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 51909.4044855
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 50831.0684004
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 49753.550906
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 48676.9763749
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 47601.4643963
+sleep 4
+./tc.sh set eth0 294688.629343
+./tc.sh set wlan0 198627.129907
+sleep 4
+./tc.sh set eth0 289621.464915
+./tc.sh set wlan0 197554.083323
+sleep 4
+./tc.sh set eth0 284560.882905
+./tc.sh set wlan0 196482.430662
+sleep 4
+./tc.sh set eth0 279507.363802
+./tc.sh set wlan0 195412.273675
+sleep 4
+./tc.sh set eth0 274461.368517
+./tc.sh set wlan0 194343.709968
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 41176.8331217
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 40111.732814
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 39048.4949355
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 37987.2017052
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 36927.931783
+sleep 4
+./tc.sh set eth0 244366.88435
+./tc.sh set wlan0 187970.76038
+sleep 4
+./tc.sh set eth0 239384.935116
+./tc.sh set wlan0 186915.759365
+sleep 4
+./tc.sh set eth0 234413.559048
+./tc.sh set wlan0 185862.997374
+sleep 4
+./tc.sh set eth0 229453.06546
+./tc.sh set wlan0 184812.539909
+sleep 4
+./tc.sh set eth0 224503.749352
+./tc.sh set wlan0 183764.449439
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 30618.7855006
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 29575.6047927
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 28534.9612689
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 27496.9062291
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 26461.4884083
+sleep 4
+./tc.sh set eth0 195057.410074
+./tc.sh set wlan0 177528.754062
+sleep 4
+./tc.sh set eth0 190193.488081
+./tc.sh set wlan0 176498.747052
+sleep 4
+./tc.sh set eth0 185342.641368
+./tc.sh set wlan0 175471.508925
+sleep 4
+./tc.sh set eth0 180505.055581
+./tc.sh set wlan0 174447.078993
+sleep 4
+./tc.sh set eth0 175680.906176
+./tc.sh set wlan0 173425.494413
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 20306.7902588
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 19290.9995935
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 18278.1535425
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 17268.2813602
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 16261.4104971
+sleep 4
+./tc.sh set eth0 147026.802917
+./tc.sh set wlan0 167357.566664
+sleep 4
+./tc.sh set eth0 142300.837066
+./tc.sh set wlan0 166356.773896
+sleep 4
+./tc.sh set eth0 137589.384881
+./tc.sh set wlan0 165359.05461
+sleep 4
+./tc.sh set eth0 132892.544867
+./tc.sh set wlan0 164364.429666
+sleep 4
+./tc.sh set eth0 128210.408445
+./tc.sh set wlan0 163372.918423
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 83062.5187768
+./tc.sh set wlan0 152675.990975
+sleep 4
+./tc.sh set eth0 79620.7228451
+./tc.sh set wlan0 151722.878255
+sleep 4
+./tc.sh set eth0 76190.6690701
+./tc.sh set wlan0 150773.01721
+sleep 4
+./tc.sh set eth0 72772.3697688
+./tc.sh set wlan0 149826.41125
+sleep 4
+./tc.sh set eth0 69365.8342392
+./tc.sh set wlan0 148883.062949
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 16458.7475342
+./tc.sh set wlan0 134231.869708
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 133343.63126
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 132458.587018
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 131576.728895
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 130698.048457
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
+./tc.sh set eth0 100
+./tc.sh set wlan0 100
+sleep 4
